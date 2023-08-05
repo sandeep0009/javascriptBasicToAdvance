b369d40671c92d1a6b1de9eafb1df2f834734a0b
Sandeep <500sandeepks@gmail.com>
2023-08-05 21:56:38 +0530

    datatypes added

---
+++ DataType.js
@@ -0,0 +1,35 @@
+/*
+JavaScript is a dynamically-typed language, which means that variables can hold values of different data types.
+Data types in javascript is of two type : 
+        1-Primitive data types
+        2- Refrence data types
+
+        Primitve data types:
+        javascript have follwoing primitive types :
+            1- NaN (not a number )
+            2- String
+            3- Bollean
+            4- Undefined 
+            5- Null
+
+        Reference data types :
+            1- Array
+            2- Object
+            3- Function
+        
+        Special data types : immutable data types used to create private objects 
+
+        infinity:positive infinity
+        -infinity:negative infinity
+        nan:not a number
+
+        
+*/
+let a="sandeep";
+console.log(typeof(a));
+let b=true;
+console.log(typeof(b));
+let c;
+console.log(typeof(c));
+let d=null;
+console.log(d)
