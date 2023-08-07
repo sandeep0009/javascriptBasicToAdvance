8fe390d626a8cde2e50a6351def571617d02065c
Sandeep <500sandeepks@gmail.com>
2023-08-07 10:03:52 +0530

    operators added

---
+++ Operators.js
@@ -0,0 +1,19 @@
+/*
+
+Operators in javascripts are operations performed on two operands 
+example : 2+3
+here 2 and 3 are operands and '+' is operator.
+
+In javascript there are 
+
+            Arthimatheic =>
+            1- Assignment operators
+            2- Comprison operators
+            3- Logical operators
+            4- Ternary operators
+            5- Urinary operators
+*/
+
+let a =10;
+let b=20;
+console.log(a+b);
