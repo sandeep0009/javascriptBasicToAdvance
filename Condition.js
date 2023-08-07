8e674cf7c09faf4ada024e2860cabcdf84c14494
Sandeep <500sandeepks@gmail.com>
2023-08-07 10:42:45 +0530

    added conditions

---
+++ Condition.js
@@ -0,0 +1,32 @@
+/* 
+conditional statements make you take decision on your own code basic of condition.
+    if
+    if-else
+    if else if else
+    switch
+ */
+
+
+    //example of if-else :
+    let a=15
+    if(a<18){
+        console.log("not an adult")
+    }
+    else{
+        console.log("is an adult")
+    }
+
+    //example of switch case:
+    let day="monday"
+    switch(day){
+        case "monday":
+            console.log("week first day")
+            break;
+        case "friday":
+            console.log("end of week")
+            break;
+         default:
+            console.log("same days");
+
+
+    }
