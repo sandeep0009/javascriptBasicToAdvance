dc3eaaf1285e1209a3e0d36f5818e2db2f486891
Sandeep <500sandeepks@gmail.com>
2023-08-09 00:13:03 +0530

    higher order functions are added

--- HigherOrderFunctions.js
+++ HigherOrderFunctions.js
@@ -11,3 +11,22 @@
 const arr=[1,2,3,4];
 const square=arr.map(arr=>arr*arr);
 console.log(square);
+
+
+//filter returns a new array which pass the condition provided
+
+const arr1=[1,2,3,4,5,6]
+const ar=arr.filter(arr1=>arr1%2==0)
+console.log(ar);
+
+//reduce reduces the array into single value
+const a=[1,2,4,5,6];
+const sum=a.reduce((acc,a)=>acc+a,0);
+console.log(sum)
+
+//forEach iterates over each element of array and applies to each element
+const b=[1,2,3,4,5];
+b.forEach(element => {
+    console.log(element)
+    
+});
